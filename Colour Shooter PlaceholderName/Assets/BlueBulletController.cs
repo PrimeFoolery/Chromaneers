aa572f2c3e069a6f83c8993eb1b981543053455e
MohamadAliAwarkeh <mohamad.awarkeh@gmail.com>
2018-02-17 17:06:18 +0000

    Writing code [WIP]

---
+++ Colour Shooter PlaceholderName/Assets/BlueBulletController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueBulletController : MonoBehaviour {
+
+    [Header ("Bullet variables")]
+    public float speed;
+    public float bulletLifeTime;
+    public int bulletDamage;
+	
+	void Update () {
+        //Moving the bullet
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        //Bullets lifeTime
+        bulletLifeTime -= Time.deltaTime;
+        //Destroying the bullet after its time has reached 0
+        if (bulletLifeTime <= 0) {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter (Collision theCol) {
+        //Check the enemy
+        if (theCol.gameObject.tag == "BlueEnemy") {
+            //When it collides with the enemy, apply the damage
+
+        }
+    }
+}
