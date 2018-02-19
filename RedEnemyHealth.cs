bacb57008cbf57d9256c6b881027afc891f3f963
weaseline <36544986+weaseline@users.noreply.github.com>
2018-02-19 17:26:01 +0000

    Test to see if upload - shield enemy

---
+++ RedEnemyHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedEnemyHealth : MonoBehaviour {
+
+	public int health;
+
+	//Private variables
+	private int currentHealth;
+
+	void Start () {
+		//Setting the current health to be the health variable
+		//so that when we start the game, the enemy has full HP
+		currentHealth = health;
+	}
+
+	void Update () {
+		//If the enemy reaches 0 HP, destroy the enemy
+		if (currentHealth <= 0) {
+			Destroy(gameObject);
+		}
+	}
+
+	//Used to call this void in the bullet scripts
+	//since currentHealth is a private variable
+	public void EnemyDamaged (int damage) {
+		currentHealth -= damage;
+	}
+}
