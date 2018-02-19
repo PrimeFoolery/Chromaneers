eff65247130421acc1ef8d8895d390c30f56530b
weaseline <36544986+weaseline@users.noreply.github.com>
2018-02-19 23:04:45 +0000

    Made scene for walking around

---
+++ Colour Shooter PlaceholderName/Assets/Scripts/Enemies/StandardEnemyBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StandardEnemyBehaviour : MonoBehaviour {
+
+    public GameObject Player;
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(Player.transform.position);
+	}
+}
