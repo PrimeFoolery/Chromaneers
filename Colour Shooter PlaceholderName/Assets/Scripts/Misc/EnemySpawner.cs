f4f1bf2964c8118271450c15ffb58f699acde5ff
weaseline <36544986+weaseline@users.noreply.github.com>
2018-02-20 14:14:24 +0000

    EnemySpawner

---
+++ Colour Shooter PlaceholderName/Assets/Scripts/Misc/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public float Timer;
+
+	public GameObject RedEnemy;
+	public GameObject YellowEnemy;
+	public GameObject BlueEnemy;
+
+	private int RanNumb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Timer += Time.deltaTime;
+
+		if (Timer >= 2) {
+			
+			RanNumb = Random.Range (0, 3);
+			if (RanNumb == 0) {
+				Instantiate (RedEnemy);
+				Timer = 0;
+			}
+			if (RanNumb == 1) {
+				Instantiate (YellowEnemy);
+				Timer = 0;
+			}
+			if (RanNumb == 2) {
+				Instantiate (BlueEnemy);
+				Timer = 0;
+			}
+
+		}
+	}
+}
