2746e5350eadd4ae24523da92bf31a8d61be2d52
weaseline <36544986+weaseline@users.noreply.github.com>
2018-02-22 21:28:52 +0000

    Shield started

---
+++ Colour Shooter PlaceholderName/Assets/Scripts/Enemies/ShieldBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldBehaviour : MonoBehaviour {
+
+    int randomColour;
+
+    public Material Yellow;
+    public Material Red;
+    public Material Blue;
+	// Use this for initialization
+	void Start () {
+        randomColour = Random.Range(0, 3);
+        if(randomColour == 0)
+        {
+            this.tag = "YellowEnemy";
+        }
+        if (randomColour == 1)
+        {
+            this.tag = "RedEnemy";
+        }
+        if (randomColour == 2)
+        {
+            this.tag = "BlueEnemy";
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
