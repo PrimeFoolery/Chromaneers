675b95762d5debd0f57a506aa55bedd1946152a7
Mohamad Ali Awarkeh <mohamad.awarkeh@gmail.com>
2018-02-26 14:33:39 +0000

    slight changes

---
+++ Colour Shooter PlaceholderName/Assets/Scripts/Singleplayer/SingleplayerHealthController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleplayerHealthController : MonoBehaviour {
+
+	public int Health;
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
