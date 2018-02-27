d11c30aafa428ed80147e7f81f63a525e84ebe75
BriggzyJ97 <briggs-97@live.co.uk>
2018-02-27 12:11:58 +0000

    ditto

--- Colour Shooter PlaceholderName/Assets/Scripts/Enemies/Standard Enemies/OrangeEnemyHealth.cs
+++ Colour Shooter PlaceholderName/Assets/Scripts/Enemies/Standard Enemies/OrangeEnemyHealth.cs
@@ -4,28 +4,63 @@ using UnityEngine;
 
 public class OrangeEnemyHealth : MonoBehaviour {
 
-    public int health;
+	//Private variables
+	public int redHealth=3;
+	public int yellowHealth=3;
+	private float recoveryTimer = 5f;
+	public GameObject sphere;
 
-    //Private variables
-    private int currentHealth;
+	public Material redJellyMaterial;
+	public Material yellowJellyMaterial;
+	public Material orangeJellyMaterial;
 
-    void Start () {
-        //Setting the current health to be the health variable
-        //so that when we start the game, the enemy has full HP
-        currentHealth = health;
-    }
+	void Start () {
+		//Setting the current health to be the health variable
+		//so that when we start the game, the enemy has full HP
+	}
 
-    void Update () {
-        //If the enemy reaches 0 HP, destroy the enemy
-        if (currentHealth <= 0) {
-            Destroy(gameObject);
-        }
-    }
+	void Update () {
+		//If the enemy reaches 0 HP, destroy the enemy
+		if(redHealth<=0&&yellowHealth>0){
+			GetComponent<Renderer> ().material = yellowJellyMaterial;
+			sphere.GetComponent<Renderer> ().material = yellowJellyMaterial;
+		}
+		if(redHealth<0){
+			redHealth=0;
+		}
+		if(yellowHealth<=0&&redHealth>0){
+			GetComponent<Renderer> ().material = redJellyMaterial;
+			sphere.GetComponent<Renderer> ().material = redJellyMaterial;
+		}
+		if(yellowHealth<0){
+			yellowHealth=0;
+		}
+		if(yellowHealth<=0&&redHealth<=0){
+			Destroy (this.gameObject);
+		}
+		recoveryTimer -= Time.deltaTime;
+		if(recoveryTimer<=0){
+			redHealth = 3;
+			yellowHealth = 3;
+			GetComponent<Renderer> ().material = orangeJellyMaterial;
+			sphere.GetComponent<Renderer> ().material = orangeJellyMaterial;
+		}
+	}
 
-    //Used to call this void in the bullet scripts
-    //since currentHealth is a private variable
-    public void EnemyDamaged (int damage) {
-        currentHealth -= damage;
+	//Used to call this void in the bullet scripts
+	//since currentHealth is a private variable
+	public void EnemyDamaged (int damage) {
+		//currentHealth -= damage;
 		GetComponent<ParticleSystem> ().Play ();
-    }
+	}
+	public void OnCollisionEnter (Collision other){
+		if(other.gameObject.CompareTag("RedBullet")){
+			redHealth -= 1;
+			recoveryTimer = 5f;
+		}
+		if(other.gameObject.CompareTag("YellowBullet")){
+			yellowHealth -= 1;
+			recoveryTimer = 5f;
+		}
+	}
 }
