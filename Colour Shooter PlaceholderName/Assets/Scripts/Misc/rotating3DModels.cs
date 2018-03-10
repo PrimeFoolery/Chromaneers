ad7beaf01f79fa90e3b2ad6018e9d8bf3c192b92
MohamadAliAwarkeh <mohamad.awarkeh@gmail.com>
2018-03-10 15:40:42 +0000

    Testing

---
+++ Colour Shooter PlaceholderName/Assets/Scripts/Misc/rotating3DModels.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class rotating3DModels : MonoBehaviour {
+
+    public float yRotation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.Rotate(Vector3.forward * yRotation * Time.deltaTime);
+	}
+}
