212f56e4f6cf9acabe4f99e95dc20229515cbc32
BriggzyJ97 <briggs-97@live.co.uk>
2018-03-10 13:29:13 +0000

    texture scaling a little better

--- Colour Shooter PlaceholderName/Assets/Scripts/Singleplayer/SingleplayerCharacterController.cs
+++ Colour Shooter PlaceholderName/Assets/Scripts/Singleplayer/SingleplayerCharacterController.cs
@@ -208,8 +208,10 @@ public class SingleplayerCharacterController : MonoBehaviour {
 	    if (Physics.Raycast(ray, out hit, 20f))
 	    {
             Debug.Log(hit.collider.name);
-	        float groundSizeX = hit.collider.gameObject.transform.localScale.x;
-	        float neededBrushSize = (-0.191f * Mathf.Log(groundSizeX, Mathf.Exp(1))) + 0.8595f;
+	        float groundSizeX = hit.collider.gameObject.GetComponent<Renderer>().bounds.size.x;
+            Debug.Log("groundSizeX:  "+groundSizeX);
+	        //float neededBrushSize = (-0.191f * Mathf.Log(groundSizeX, Mathf.Exp(1))) + 0.8595f;
+	        float neededBrushSize = 4.8228f * (Mathf.Pow(groundSizeX, -0.982f));
             Debug.Log("brush size:  "+neededBrushSize);
 	        brush.Scale = neededBrushSize;
 	        if (hit.collider)
