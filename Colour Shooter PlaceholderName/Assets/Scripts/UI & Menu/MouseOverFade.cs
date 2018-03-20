efa0a33673757d4329a48758050473ba117f9453
weaseline <36544986+weaseline@users.noreply.github.com>
2018-03-20 11:45:51 +0000

    Changed MenuTest scene

---
+++ Colour Shooter PlaceholderName/Assets/Scripts/UI & Menu/MouseOverFade.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MouseOverFade : MonoBehaviour {
+
+    public Text buttonText;
+
+	// Use this for initialization
+	void Start () {
+        buttonText.GetComponent<Text>().color = new Color(0, 0, 0, 0.5f);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+    }
+    void OnPointerEnter(PointerEventData eventData)
+    {
+        print("OVER");
+        buttonText.GetComponent<Text>().color = new Color (0, 0, 0, 1f);
+    }
+    void OnPointerExit(PointerEventData eventData)
+    {
+        print("EXIT");
+        buttonText.GetComponent<Text>().color = new Color(0, 0, 0, 0.5f);
+    }
+}
