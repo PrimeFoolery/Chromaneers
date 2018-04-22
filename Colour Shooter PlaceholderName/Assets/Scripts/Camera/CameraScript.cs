ee757b5f122d54273e425170f8a349e7a9631abb
BriggzyJ97 <briggs-97@live.co.uk>
2018-04-22 21:34:13 +0100

    different camera options

--- Colour Shooter PlaceholderName/Assets/Scripts/Camera/CameraScript.cs
+++ Colour Shooter PlaceholderName/Assets/Scripts/Camera/CameraScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 
 public class CameraScript : MonoBehaviour
 {
@@ -9,8 +10,20 @@ public class CameraScript : MonoBehaviour
     public GameState currentGameState = GameState.Multiplayer;
     private ColourSelectManager gameManager;
 
+    private enum CameraState
+    {
+        TopDown,
+        SixtyDegrees,
+        FiftyDegrees,
+        SixtyDegreesSlanted,
+        FiftyDegreesSlanted
+    };
+
+    private CameraState currentCameraState = CameraState.SixtyDegrees;
+
     private float cameraMoveSpeed = 0.35f;//SPEED THAT CAMERA MOVES TOWARDS TARGET POSITION
     private Vector3 targetCameraPosition;// THE POSITION OF THE CAMERA
+    private Vector3 targetCameraRotation;
     private Camera cameraComponent;
 
     //SINGLEPLAYER VARIABLES
@@ -32,6 +45,7 @@ public class CameraScript : MonoBehaviour
     private float aspectRatio;
     private float zoomSpeed = 4f;
     private float dampTime = 0.2f;
+    private float rotationTime = 0.35f;
 
     //CAMERA SHAKE VARIABLES
     public float shakeDuration = 0f;
@@ -86,10 +100,37 @@ public class CameraScript : MonoBehaviour
             normMousePos = Camera.main.ScreenToWorldPoint(rawMousePos);
             CalculateAveragePosInSinglePlayer();
             Vector3 velocity = Vector3.zero;
-            targetCameraPosition = new Vector3(averagePos.x, averagePos.y + 15f, averagePos.z - 7.5f);//The target Position above player is calculated
+            if (currentCameraState==CameraState.SixtyDegrees)
+            {
+                targetCameraPosition = new Vector3(averagePos.x, averagePos.y + 45f, averagePos.z - 27.5f);
+                targetCameraRotation = new Vector3(60,0,0);
+            }
+            else if (currentCameraState == CameraState.SixtyDegreesSlanted)
+            {
+                targetCameraPosition = new Vector3(averagePos.x-5f, averagePos.y + 45f, averagePos.z - 27.5f);
+                targetCameraRotation = new Vector3(60, 15, 0);
+            }
+            else if (currentCameraState == CameraState.FiftyDegrees)
+            {
+                targetCameraPosition = new Vector3(averagePos.x, averagePos.y + 45f, averagePos.z - 40f);
+                targetCameraRotation = new Vector3(50, 0, 0);
+            }
+            else if (currentCameraState == CameraState.FiftyDegreesSlanted)
+            {
+                targetCameraPosition = new Vector3(averagePos.x-6f, averagePos.y + 45f, averagePos.z - 37.5f);
+                targetCameraRotation = new Vector3(50, 10, 0);
+            }
+            else if (currentCameraState == CameraState.TopDown)
+            {
+                targetCameraPosition = new Vector3(averagePos.x, averagePos.y + 45f, averagePos.z - 10f);
+                targetCameraRotation = new Vector3(80, 0, 0);
+            }
+            //The target Position above player is calculated
             //transform.position = Vector3.MoveTowards(transform.position, targetCameraPosition, cameraMoveSpeed);//The Camera always moves smoothly towards
             //transform.position = targetCameraPosition;
-
+            Vector3 tempCurrentRotation = transform.rotation.eulerAngles;
+            Vector3 tempRotation = Vector3.SmoothDamp(tempCurrentRotation, targetCameraRotation, ref velocity, rotationTime);
+            transform.rotation = Quaternion.Euler(tempRotation);
             transform.position = Vector3.SmoothDamp(transform.position, targetCameraPosition, ref velocity, cameraMoveSpeed);
             //averagePos.y = 0;
 
@@ -102,8 +143,37 @@ public class CameraScript : MonoBehaviour
         {
             CalculateAveragePosInCoOp();//CALCULATE THE AVERAGE POSITION BETWEEN ALL 3 PLAYERS
             CalculateSizeNeeded();//CALCULATE THE LEVEL OF ZOOM THAT THE CAMERA NEEDS TO BE TO FIT ALL PLAYERS ON SCREEN
-            targetCameraPosition = new Vector3(averagePos.x, averagePos.y + 15f, averagePos.z - 7.5f);//WHERE THE CAMERA SHOULD BE MOVING
+            if (currentCameraState == CameraState.SixtyDegrees)
+            {
+                targetCameraPosition = new Vector3(averagePos.x, averagePos.y + 45f, averagePos.z - 27.5f);
+                targetCameraRotation = new Vector3(60, 0, 0);
+            }
+            else if (currentCameraState == CameraState.SixtyDegreesSlanted)
+            {
+                targetCameraPosition = new Vector3(averagePos.x - 5f, averagePos.y + 45f, averagePos.z - 27.5f);
+                targetCameraRotation = new Vector3(60, 15, 0);
+            }
+            else if (currentCameraState == CameraState.FiftyDegrees)
+            {
+                targetCameraPosition = new Vector3(averagePos.x, averagePos.y + 45f, averagePos.z - 40f);
+                targetCameraRotation = new Vector3(50, 0, 0);
+            }
+            else if (currentCameraState == CameraState.FiftyDegreesSlanted)
+            {
+                targetCameraPosition = new Vector3(averagePos.x - 6f, averagePos.y + 45f, averagePos.z - 37.5f);
+                targetCameraRotation = new Vector3(50, 10, 0);
+            }
+            else if (currentCameraState == CameraState.TopDown)
+            {
+                targetCameraPosition = new Vector3(averagePos.x, averagePos.y + 45f, averagePos.z - 10f);
+                targetCameraRotation = new Vector3(80, 0, 0);
+            }
+            Debug.Log(targetCameraRotation);
+            Debug.Log(currentCameraState);
             Vector3 velocity = Vector3.zero;
+            Vector3 tempCurrentRotation = transform.rotation.eulerAngles;
+            Vector3 tempRotation = Vector3.SmoothDamp(tempCurrentRotation, targetCameraRotation, ref velocity, rotationTime);
+            transform.rotation = Quaternion.Euler(tempRotation);
             transform.position = Vector3.SmoothDamp(transform.position, targetCameraPosition, ref velocity, cameraMoveSpeed);
             //transform.position = Vector3.MoveTowards(transform.position, targetCameraPosition, cameraMoveSpeed);//MOVING THE CAMERA TOWARDS THE TARGET POS FOR IT
             //transform.LookAt(new Vector3(averagePos.x, 0, averagePos.z));//HAVING THE CAMERA LOOK AT THE TARGET POS
@@ -126,6 +196,27 @@ public class CameraScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.T))
         {
             //BigScreenShake();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            currentCameraState = CameraState.SixtyDegrees;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            currentCameraState = CameraState.SixtyDegreesSlanted;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            currentCameraState = CameraState.FiftyDegrees;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            currentCameraState = CameraState.FiftyDegreesSlanted;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            currentCameraState = CameraState.TopDown;
         }
     }
     private void CalculateAveragePosInCoOp()
