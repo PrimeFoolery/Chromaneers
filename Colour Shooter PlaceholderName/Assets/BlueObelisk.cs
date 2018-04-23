b6bde1ba017e511053cfaae87f9ed7809dddadbc
BriggzyJ97 <briggs-97@live.co.uk>
2018-04-23 15:22:58 +0100

    Add files via upload

---
+++ Colour Shooter PlaceholderName/Assets/BlueObelisk.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueObelisk : MonoBehaviour {
+
+	private int obeliskHealth = 3;
+	private bool heartSpawned = false;
+	public GameObject heart;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(obeliskHealth<=0){
+			if(transform.localScale.z>-0.1f){
+				transform.localScale += new Vector3 (0, 0, -0.2f);
+
+			}else if(transform.localScale.z<=-0.1f){
+				if(heartSpawned==false){
+					Instantiate (heart, new Vector3(transform.position.x,transform.position.y+1f,transform.position.z), Quaternion.identity);
+					gameObject.GetComponent<BoxCollider> ().enabled = false;
+					heartSpawned = true;
+				}
+				transform.localScale = new Vector3 (3f, 3f, -0.1f);
+			}
+		}
+	}
+	void OnCollisionEnter (Collision other){
+		if(other.collider.CompareTag("BlueBullet")){
+			
+			DamageBlue ();
+			Destroy (other.gameObject);
+		}
+	}
+	public void DamageBlue(){
+		obeliskHealth -= 1;
+	}
+}
---
+++ Colour Shooter PlaceholderName/Assets/heartScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heartScript : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter (Collision other){
+		if(other.gameObject.CompareTag("BluePlayer")){
+			other.gameObject.GetComponent<CoopCharacterHealthControllerOne> ().GetHeart ();
+		}else
+		if(other.gameObject.CompareTag("RedPlayer")){
+			other.gameObject.GetComponent<CoopCharacterHealthControllerTwo> ().GetHeart ();
+		}else
+		if(other.gameObject.CompareTag("YellowPlayer")){
+			other.gameObject.GetComponent<CoopCharacterHealthControllerThree> ().GetHeart ();
+		}
+		Destroy (gameObject);
+	}
+}
