9f8fdcc426f84f2cd1066f5924cf122ac8b43dbf
MohamadAliAwarkeh <mohamad.awarkeh@gmail.com>
2018-04-25 19:06:52 +0100

    Update

--- Colour Shooter PlaceholderName/Assets/Scripts/Bullets/BlueBulletController.cs
+++ Colour Shooter PlaceholderName/Assets/Scripts/Bullets/BlueBulletController.cs
@@ -181,6 +181,8 @@ public class BlueBulletController : MonoBehaviour {
             transform.localScale -= new Vector3(0.2f, 0.2f, 0.2f);
             //Change trail width
             this.GetComponent<TrailRenderer>().startWidth = 0.3f;
+            //Play audio
+            this.GetComponent<AudioSource>().Play();
         }
 		/*
 		if (theCol.gameObject.CompareTag("RedBullet")) {
