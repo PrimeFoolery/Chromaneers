7c9bb62b8fb395074e49fc752d36011569006c7c
BriggzyJ97 <briggs-97@live.co.uk>
2018-04-28 20:38:26 +0100

    made vial better

--- Colour Shooter PlaceholderName/Assets/VialDoorController.cs
+++ Colour Shooter PlaceholderName/Assets/VialDoorController.cs
@@ -9,11 +9,11 @@ public class VialDoorController : MonoBehaviour
     public GameObject middleVial;
     public GameObject rightVial;
 
-    private Vector3 leftVialTargetTransform;
-    private Vector3 middleVialTargetTransform;
-    private Vector3 rightVialTargetTransform;
+    public Transform leftVialTargetTransform;
+    public Transform middleVialTargetTransform;
+    public Transform rightVialTargetTransform;
 
-    private Vector3 doorTargetTransform;
+    public Transform doorTargetTransform;
 
     public float vialMoveSpeed = 1f;
     public float doorMoveSpeed = 3f;
@@ -22,32 +22,32 @@ public class VialDoorController : MonoBehaviour
 
 	// Use this for initialization
 	void Start () {
-		leftVialTargetTransform = new Vector3(leftVial.transform.position.x, leftVial.transform.position.y, leftVial.transform.position.z+2f);
-	    middleVialTargetTransform = new Vector3(middleVial.transform.position.x, middleVial.transform.position.y , middleVial.transform.position.z+2f);
-	    rightVialTargetTransform = new Vector3(rightVial.transform.position.x, rightVial.transform.position.y , rightVial.transform.position.z+2f);
+		//leftVialTargetTransform = new Vector3(leftVial.transform.position.x, leftVial.transform.position.y, leftVial.transform.position.z+2f);
+	    //middleVialTargetTransform = new Vector3(middleVial.transform.position.x, middleVial.transform.position.y , middleVial.transform.position.z+2f);
+	    //rightVialTargetTransform = new Vector3(rightVial.transform.position.x, rightVial.transform.position.y , rightVial.transform.position.z+2f);
 
-        doorTargetTransform = new Vector3(transform.position.x-22.5f, transform.position.y, transform.position.z);
+        //doorTargetTransform = new Vector3(transform.position.x-22.5f, transform.position.y, transform.position.z);
     }
 	
 	// Update is called once per frame
 	void Update () {
-	    if (leftVial.GetComponentInChildren<VialController>().VialCorrectlyFilled==true && Vector3.Distance(leftVial.transform.position, leftVialTargetTransform)>0f && doorFullyFilled==false)
+	    if (leftVial.GetComponentInChildren<VialController>().VialCorrectlyFilled==true && Vector3.Distance(leftVial.transform.position, leftVialTargetTransform.position)>0f )
 	    {
-	        leftVial.transform.position = Vector3.MoveTowards(leftVial.transform.position, leftVialTargetTransform,vialMoveSpeed * Time.deltaTime);
+	        leftVial.transform.position = Vector3.MoveTowards(leftVial.transform.position, leftVialTargetTransform.position,vialMoveSpeed * Time.deltaTime);
 	    }
-	    if (middleVial.GetComponentInChildren<VialController>().VialCorrectlyFilled == true && Vector3.Distance(middleVial.transform.position, middleVialTargetTransform) > 0f && doorFullyFilled==false)
+	    if (middleVial.GetComponentInChildren<VialController>().VialCorrectlyFilled == true && Vector3.Distance(middleVial.transform.position, middleVialTargetTransform.position) > 0f )
 	    {
-	        middleVial.transform.position = Vector3.MoveTowards(middleVial.transform.position, middleVialTargetTransform, vialMoveSpeed * Time.deltaTime);
+	        middleVial.transform.position = Vector3.MoveTowards(middleVial.transform.position, middleVialTargetTransform.position, vialMoveSpeed * Time.deltaTime);
 	    }
-	    if (rightVial.GetComponentInChildren<VialController>().VialCorrectlyFilled == true && Vector3.Distance(rightVial.transform.position, rightVialTargetTransform) > 0f && doorFullyFilled == false)
+	    if (rightVial.GetComponentInChildren<VialController>().VialCorrectlyFilled == true && Vector3.Distance(rightVial.transform.position, rightVialTargetTransform.position) > 0f )
 	    {
-	        rightVial.transform.position = Vector3.MoveTowards(rightVial.transform.position, rightVialTargetTransform, vialMoveSpeed * Time.deltaTime);
+	        rightVial.transform.position = Vector3.MoveTowards(rightVial.transform.position, rightVialTargetTransform.position, vialMoveSpeed * Time.deltaTime);
 	    }
 
 	    if (leftVial.GetComponentInChildren<VialController>().VialCorrectlyFilled == true && middleVial.GetComponentInChildren<VialController>().VialCorrectlyFilled == true && (rightVial.GetComponentInChildren<VialController>().VialCorrectlyFilled == true))
 	    {
 	        doorFullyFilled = true;
-	        transform.position = Vector3.MoveTowards(transform.position, doorTargetTransform, doorMoveSpeed * Time.deltaTime);
+	        transform.position = Vector3.MoveTowards(transform.position, doorTargetTransform.position, doorMoveSpeed * Time.deltaTime);
 	    }
     }
 }
