1904bf227eccf994735d2eba46de825f38aa1ca5
BriggzyJ97 <briggs-97@live.co.uk>
2018-05-03 17:53:43 +0100

    destructibles are better and on map

--- Colour Shooter PlaceholderName/Assets/Scripts/Misc/heartScript.cs
+++ Colour Shooter PlaceholderName/Assets/Scripts/Misc/heartScript.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class heartScript : MonoBehaviour
 {
-
+    private int heartHealth = 3;
     private float YPosition;
+    public GameObject dustExplosion;
 
 	// Use this for initialization
 	void Start ()
@@ -17,17 +18,32 @@ public class heartScript : MonoBehaviour
 	void Update () {
 		transform.Rotate(0,7,0);
 	    transform.position = new Vector3(transform.position.x, YPosition+Mathf.PingPong(Time.time, 1f), transform.position.z);
+
+	    if (heartHealth<=0)
+	    {
+	        Instantiate(dustExplosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+	    }
     }
 	void OnCollisionEnter (Collision other){
 		if(other.gameObject.CompareTag("BluePlayer")){
 			other.gameObject.GetComponent<CoopCharacterHealthControllerOne> ().GetHeart ();
-		}else
+		    Destroy(gameObject);
+        }
+        else
 		if(other.gameObject.CompareTag("RedPlayer")){
 			other.gameObject.GetComponent<CoopCharacterHealthControllerTwo> ().GetHeart ();
-		}else
+		    Destroy(gameObject);
+        }
+        else
 		if(other.gameObject.CompareTag("YellowPlayer")){
 			other.gameObject.GetComponent<CoopCharacterHealthControllerThree> ().GetHeart ();
+            Destroy (gameObject);
+		} else if (other.gameObject.CompareTag("BlueBullet")||other.gameObject.CompareTag("RedBullet")||other.gameObject.CompareTag("YellowBullet")||other.gameObject.CompareTag("RainbowBullet"))
+		{
+		    heartHealth -= 1;
+            Destroy(other.gameObject);
 		}
-		Destroy (gameObject);
+		
 	}
 }
