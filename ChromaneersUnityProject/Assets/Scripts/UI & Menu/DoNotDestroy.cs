7661197f53eba385b9a90c6d8847d4c99d40084f
weaseline <36544986+weaseline@users.noreply.github.com>
2018-05-20 23:07:24 +0100

    Animation

--- ChromaneersUnityProject/Assets/Scripts/UI & Menu/DoNotDestroy.cs
+++ ChromaneersUnityProject/Assets/Scripts/UI & Menu/DoNotDestroy.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DoNotDestroy : MonoBehaviour {
 
@@ -9,20 +10,41 @@ public class DoNotDestroy : MonoBehaviour {
     public bool highResWaterHolder;
     public float volumeHolder;
     public GameObject volumeSliderHolder;
+    Scene currentScene;
+
+    public static DoNotDestroy Instance;
 
     private void Awake()
     {
-        DontDestroyOnLoad(this.gameObject);
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Use this for initialization
     void Start () {
+        Scene currentScene = SceneManager.GetActiveScene();
         canvasHolder = GameObject.FindGameObjectWithTag("Canvas");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        highResWaterHolder = canvasHolder.GetComponent<MainMenu>().highResWater;
-        volumeHolder = volumeSliderHolder.GetComponent<Slider>().value; 
+        string sceneName = currentScene.name;
+
+        if (sceneName == "MenuTest")
+        {
+            highResWaterHolder = canvasHolder.GetComponent<MainMenu>().highResWater;
+            volumeHolder = canvasHolder.GetComponent<MainMenu>().buttonListOpt[6].GetComponent<Slider>().value;
+        }
+        if(sceneName == "NewWorld")
+        {
+
+        }
 	}
 }
