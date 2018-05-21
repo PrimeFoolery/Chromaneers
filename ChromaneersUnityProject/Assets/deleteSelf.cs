6a231f581d6c5efc86d28fc570b2a14d9811ba40
weaseline <36544986+weaseline@users.noreply.github.com>
2018-05-21 10:42:40 +0100

    Death particle

--- ChromaneersUnityProject/Assets/deleteSelf.cs
+++ ChromaneersUnityProject/Assets/deleteSelf.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class deleteSelf : MonoBehaviour {
 
     public float delay = 0f;
+    public float timer = 1f;
 
 	// Use this for initialization
 	void Start () {
         Destroy(gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + delay);
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
+        timer -= Time.deltaTime;
+        if(timer<= 0.5)
+        {
+            StartCoroutine("Fade");
+        }
+    }
+    IEnumerator Fade()
+    {
+        for (float f = 1f; f >= 0; f -= 0.04f)
+        {
+            Color c = GetComponent<SpriteRenderer>().color;
+            c.a = f;
+            GetComponent<SpriteRenderer>().color = c;
+            yield return null;
+        }
     }
 }
