f8cebffe2aed41d5c09044bf7d41d505003b83c6
BriggzyJ97 <briggs-97@live.co.uk>
2018-05-29 19:01:56 +0100

    pulsing line don but not set up

---
+++ ChromaneersUnityProject/Assets/linePulser.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linePulser : MonoBehaviour
+{
+    public string colourOfLine = "blue";
+    private Color whiteColor =  new Color(1,1,1,1);
+    private Color redColor = Color.red;
+    private Color blueColor = Color.blue;
+    private Color yellowColor = Color.yellow;
+    public int lengthOfLineRenderer = 20;
+
+    private LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    lineRenderer = gameObject.GetComponent<LineRenderer>();
+        lineRenderer .material = new Material(Shader.Find("Particles/Additive"));
+        //lineRenderer.startColor = c1;
+	    //lineRenderer.endColor = c2;
+	    lineRenderer.positionCount = lengthOfLineRenderer;
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    AnimationCurve widthCurve = new AnimationCurve();
+	    Gradient colorGradient = new Gradient();
+        int i = 0;
+	    while (i< lengthOfLineRenderer)
+	    {
+	        Vector3 pos = new Vector3(lineRenderer.GetPosition(0).x + i * 0.5f, (Mathf.Sin(i+Time.time))/100, lineRenderer.GetPosition(0).y);
+            lineRenderer.SetPosition(i, pos);
+	        float width = ((((Mathf.Sin(i+ Time.time)+1) / 2)+0.5f)/2);
+	        widthCurve.AddKey((i / 20f), width);
+	        if (colourOfLine == "blue")
+	        {
+	            colorGradient.SetKeys(
+	                new GradientColorKey[] { new GradientColorKey(blueColor, 0), new GradientColorKey(whiteColor, 1) },
+	                new GradientAlphaKey[] { new GradientAlphaKey(1, i / 20f) }
+	            );
+            }else if
+	            (colourOfLine == "yellow")
+	        {
+	            colorGradient.SetKeys(
+	                new GradientColorKey[] { new GradientColorKey(yellowColor, 0), new GradientColorKey(whiteColor, 1) },
+	                new GradientAlphaKey[] { new GradientAlphaKey(1, i / 20f) }
+	            );
+            }
+	        else if
+	            (colourOfLine == "red")
+	        {
+	            colorGradient.SetKeys(
+	                new GradientColorKey[] { new GradientColorKey(redColor, 0), new GradientColorKey(whiteColor, 1) },
+	                new GradientAlphaKey[] { new GradientAlphaKey(1, i / 20f) }
+	            );
+	        }
+
+            lineRenderer.colorGradient = colorGradient;
+            i++;
+	    }
+
+	    
+	    lineRenderer.widthCurve = widthCurve;
+	}
+}
